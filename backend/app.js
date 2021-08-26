527b547ec4203ca742786ef22b97a8f1a7dfd93a
jmorataya2019041 <jmorataya-2019041@kinal.edu.gt>
2021-08-25 18:44:06 -0600

    Se agregó la conectividad a la base de datos y el servidor remoto

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,25 @@
 'use strict'
+
+//Variables globales
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors")
+const morgan = require("morgan");
+
+//Cabeceras
+app.use(cors());
+
+//Importación de rutas
+
+
+//Middlewares
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+//Utilización de rutas
+
+
+//Exportación
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
 'use strict'
+const mongoose = require("mongoose");
+const app = require("./app");
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/bibliotecadigital', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
+    console.log("Se encuentra conectado a la base de datos");
+
+    app.listen(3000, function(){
+        console.log("Está funcionando en el puerto 3000");
+    })
+
+}).catch(err => console.log(err));
